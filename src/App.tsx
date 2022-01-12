292e8f1959e93100f915a3c19e015c3a3745e84e
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-01-12 22:34:00 +0100

    chore: setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Authenticator} from '@aws-amplify/ui-react';
+import "@aws-amplify/ui-react/styles.css"
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsExports);
+
+export default function App() {
+    return (
+        <Authenticator>
+            {
+                ({signOut, user}) =>
+                    <main>
+                        <h1>Hello {user.username}</h1>
+                        <button onClick={signOut}>Sign out</button>
+                    </main>
+            }
+        </Authenticator>
+    )
 }
-
-export default App;
