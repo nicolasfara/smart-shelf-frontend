a037c48084d3756d1f4c415cb2da5da9feb39249
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-01-18 19:07:32 +0100

    style: remove semicolons and enable double quotes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { act } from 'react-dom/test-utils';
-import { waitFor } from '@testing-library/react';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import { act } from "react-dom/test-utils"
+import { waitFor } from "@testing-library/react"
+import App from "./App"
 
-let container: ReactDOM.Container | null;
+let container: ReactDOM.Container | null
 
 beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
+  container = document.createElement("div")
+  document.body.appendChild(container)
+})
 
 afterEach(() => {
   if (container !== null) {
-    document.body.removeChild(container);
+    document.body.removeChild(container)
   }
-  container = null;
-});
+  container = null
+})
 
-test('renders learn react link', async () => {
+test("renders learn react link", async () => {
   act(() => {
-    ReactDOM.render(<App />, container);
-  });
+    ReactDOM.render(<App />, container)
+  })
   await waitFor(() => {
-    const button = container?.querySelector('button');
+    const button = container?.querySelector("button")
     if (button !== null && button !== undefined) {
-      expect(button.textContent).toBe('Sign iyn');
+      expect(button.textContent).toBe("Sign iyn")
     }
-  });
-});
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Authenticator, Button } from '@aws-amplify/ui-react';
+import React from "react"
+import { Authenticator, Button } from "@aws-amplify/ui-react"
 // eslint-disable-next-line import/no-unresolved
-import '@aws-amplify/ui-react/styles.css';
-import './App.css';
-import Amplify from 'aws-amplify';
-import awsExports from './aws-exports.js';
+import "@aws-amplify/ui-react/styles.css"
+import "./App.css"
+import Amplify from "aws-amplify"
+import awsExports from "./aws-exports.js"
 
-Amplify.configure(awsExports);
+Amplify.configure(awsExports)
 
 export default function App(): React.ReactElement {
   return (
@@ -23,5 +23,5 @@ export default function App(): React.ReactElement {
         )
       }
     </Authenticator>
-  );
+  )
 }
