d3f88496b6ff0adacfa06f1d519d2f52b99df23d
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-01-18 18:58:10 +0100

    style: refactor the project accroding to eslint config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { waitFor } from '@testing-library/react';
+import App from './App';
 
-test('renders learn react link', () => {
-  expect(10).toBeGreaterThan(9)
+let container: ReactDOM.Container | null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  if (container !== null) {
+    document.body.removeChild(container);
+  }
+  container = null;
+});
+
+test('renders learn react link', async () => {
+  act(() => {
+    ReactDOM.render(<App />, container);
+  });
+  await waitFor(() => {
+    const button = container?.querySelector('button');
+    if (button !== null && button !== undefined) {
+      expect(button.textContent).toBe('Sign iyn');
+    }
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import {Authenticator, Button} from '@aws-amplify/ui-react';
-import "@aws-amplify/ui-react/styles.css"
-import "./App.css"
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
+import { Authenticator, Button } from '@aws-amplify/ui-react';
+// eslint-disable-next-line import/no-unresolved
+import '@aws-amplify/ui-react/styles.css';
+import './App.css';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports.js';
 
 Amplify.configure(awsExports);
 
-export default function App() {
-    return (
-        <Authenticator>
-            {
-                ({signOut, user}) =>
-                    <main>
-                        <h1>Hello {user.username}</h1>
-                        <Button onClick={signOut}>Sign out</Button>
-                    </main>
-            }
-        </Authenticator>
-    )
+export default function App(): React.ReactElement {
+  return (
+    <Authenticator>
+      {
+        ({ signOut, user }) => (
+          <main>
+            <h1>
+              Hello
+              {user.username}
+            </h1>
+            <Button id="signinBtn" onClick={signOut}>Sign out</Button>
+          </main>
+        )
+      }
+    </Authenticator>
+  );
 }
