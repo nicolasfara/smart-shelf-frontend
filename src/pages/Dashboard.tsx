6c398b46758aa5662d09338e171218fb5b65a1f5
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-01-28 18:06:32 +0100

    chore: add product list on dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,17 +2,17 @@ import React from "react"
 import Amplify from "aws-amplify"
 import { Authenticator } from "@aws-amplify/ui-react"
 import awsExports from "../aws-exports.js"
+import ProductList from "../components/products/ProductList"
 
 Amplify.configure(awsExports)
 
 export default function Dashboard(): React.ReactElement {
   return (
     <Authenticator>
-      {({ user }) => (
-        <h1>
-          hello
-          {user.username}
-        </h1>
+      {() => (
+        <div>
+          <ProductList />
+        </div>
       )}
     </Authenticator>
   )
