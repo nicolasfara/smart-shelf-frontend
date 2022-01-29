4100b003b17f43f49bb6ec5ea55a626a57deddf0
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-01-29 10:34:27 +0100

    style: add a base page layout

--- src/pages/Warehouse.tsx
+++ src/pages/Warehouse.tsx
@@ -2,16 +2,17 @@ import React from "react"
 import Amplify from "aws-amplify"
 import { Authenticator } from "@aws-amplify/ui-react"
 import awsExports from "../aws-exports.js"
+import ProductList from "../components/products/ProductList"
 import BaseLayout from "./layout/BaseLayout"
 
 Amplify.configure(awsExports)
 
-export default function Dashboard(): React.ReactElement {
+export default function Warehouse(): React.ReactElement {
   return (
     <Authenticator>
       {() => (
         <BaseLayout>
-          <h2>TODO</h2>
+          <ProductList />
         </BaseLayout>
       )}
     </Authenticator>
