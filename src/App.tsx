64cff1247e8ed28ae41645e8acf603335dd3cbe8
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-02-07 10:33:39 +0100

    fix: improve quality of shelves component and fix save operation with MQTT messagge

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ import "./App.css"
 import Amplify from "aws-amplify"
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Route, Routes } from "react-router-dom"
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { AWSIoTProvider } from "@aws-amplify/pubsub"
 import awsExports from "./aws-exports.js"
 import Dashboard from "./pages/Dashboard"
 import Warehouse from "./pages/Warehouse"
 
 Amplify.configure(awsExports)
+Amplify.addPluggable(new AWSIoTProvider({
+  aws_pubsub_region: "eu-west-1",
+  aws_pubsub_endpoint: "wss://a2d3g7wnaaw7gh-ats.iot.eu-west-1.amazonaws.com/mqtt",
+}))
 
 export default function App(): React.ReactElement {
   return (
